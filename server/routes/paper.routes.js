57bf8c25f723c498ee93c12efce699a11be013b1
Delcha <202411974@gordoncollege.edu.ph>
2026-04-09 16:40:56 +0800

    Built base Paper router (paper.routes.js)

---
+++ server/routes/paper.routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+// stubs - I'll fill these in next
+router.get('/', (req, res) => res.json({ message: 'paper routes coming soon'}))
+
+module.exports = router
