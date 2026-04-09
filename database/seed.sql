aa4ebdb454d711e09239efab6f8f2765ac8ca43f
Delcha <202411974@gordoncollege.edu.ph>
2026-04-09 14:44:19 +0800

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,8 +1,5 @@
 USE corev_db;
 
--- password for both accounts is: password 123
--- (already hashed with bcrypt, 10 salt rounds)
-
 INSERT INTO users (name, email, password, role) VALUES
 (
     'Charles De Leon',
