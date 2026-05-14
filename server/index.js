1cca2ae9b4cdf85a48f17eda48a41d9c621825ae
Delcha <202411974@gordoncollege.edu.ph>
2026-05-14 10:40:04 +0800

    Update on CORS directory to catch process.env.CLIRENT_URL

--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ console.log('DB_NAME:', process.env.DB_NAME)
 
 const app = express()
 
-app.use(cors({origin: 'http://localhost:5173'}))
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
